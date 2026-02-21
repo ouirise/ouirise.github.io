2b15150a9b2f7ea7c886d15a3dcc556a654ff138
ok <kalebkandake@gmail.com>
2026-02-21 12:22:21 -0500

    update?

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import { reactRouter } from '@react-router/dev'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRouter()],
   base: '/ouirise.github.io/', // Check!
   build: {
     outDir: 'dist',
